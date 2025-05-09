6fed98f3b7d0dce5e4e866ff04a741ca961879d4
Brian Li <brianchunghli@gmail.com>
2025-05-09 17:10:54 +1000

    feature(new): category creation for new command

--- src/core/actions/edit.rs
+++ src/core/actions/edit.rs
@@ -22,20 +22,18 @@ impl<'a> Command<'a> for Edit<'a> {
         let category = args.get_one::<String>("category").map(|s| s.as_str());
         // FIXME: Validation for categories. They should be in the format of
         // a relative directory path e.g., /some/path/
-        let path = if let Some(category) = category {
-            format!(
-                "{}/{}/{}.md",
-                conf.settings.path,
-                category,
-                name.replace(" ", "-").trim()
-            )
+        let mut path = if let Some(category) = category {
+            format!("{}/{}", conf.settings.path, category)
         } else {
-            format!(
-                "{}/{}.md",
-                conf.settings.path,
-                name.replace(" ", "-").trim()
-            )
+            conf.settings.path.clone()
         };
+        for p in name.split("/") {
+            if !p.is_empty() {
+                path.push('/');
+                path.push_str(p);
+            }
+        }
+        path.push_str(".md");
         Ok(Self {
             name: name.as_str(),
             category,
@@ -47,12 +45,23 @@ impl<'a> Command<'a> for Edit<'a> {
     }
 
     fn execute(&self) -> Result<(), Box<dyn Error>> {
-        let file = markdown::File::new(
+        // Create the category if it does not exist
+        if self.category.is_some() {
+            // The path will always be valid
+            let (parent, _) = self
+                .path
+                .rsplit_once("/")
+                .expect("create: a non valid path provided at creation");
+            if !std::fs::exists(parent)? {
+                std::fs::create_dir_all(parent)?
+            };
+        }
+        markdown::File::new(
             self.name,
             self.path.as_str(),
             self.category,
             self.tags.as_deref(),
-        );
-        file.write()
+        )
+        .write()
     }
 }
