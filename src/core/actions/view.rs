3d6fcfc8f0df648d2f820c27c0e8e86125c2b823
Brian Li <brianchunghli@gmail.com>
2025-05-19 15:39:17 +1000

    dep: add termcolor and colour write macros

--- src/core/actions/view.rs
+++ src/core/actions/view.rs
@@ -1,10 +1,61 @@
 use std::{error::Error, fs::DirEntry, ops::Deref, path::Path};
 
 use clap::ArgMatches;
+use termcolor::{Color, ColorSpec, StandardStream, WriteColor};
 
 use crate::system::Configuration;
 
 use super::Command;
+
+macro_rules! write_coloured {
+
+    // bold write
+    ($stream: ident, bold, $($arg: tt)+) => {
+        $stream.set_color(ColorSpec::new().set_bold(true))?;
+        write!($stream, $($arg)+)?;
+        $stream.reset()?;
+    };
+
+    // write coloured
+    ($stream: ident, colour=$colour: expr, $($arg: tt)+) => {
+        $stream.set_color(ColorSpec::new().set_fg(Some($colour)))?;
+        write!($stream, $($arg)+)?;
+        $stream.reset()?;
+    };
+
+    // write coloured and bolded
+    ($stream: ident, bold_colour=$colour: expr, $($arg: tt)+) => {
+        $stream.set_color(ColorSpec::new().set_bold(true).set_fg(Some($colour)))?;
+        write!($stream, $($arg)+)?;
+        $stream.reset()?;
+    };
+
+}
+
+macro_rules! write_colouredln {
+
+    // bold write
+    ($stream: ident, bold, $($arg: tt)+) => {
+        $stream.set_color(ColorSpec::new().set_bold(true))?;
+        writeln!($stream, $($arg)+)?;
+        $stream.reset()?;
+    };
+
+    // write coloured
+    ($stream: ident, colour=$colour: expr, $($arg: tt)+) => {
+        $stream.set_color(ColorSpec::new().set_fg(Some($colour)))?;
+        writeln!($stream, $($arg)+)?;
+        $stream.reset()?;
+    };
+
+    // write coloured and bolded
+    ($stream: ident, bold_colour=$colour: expr, $($arg: tt)+) => {
+        $stream.set_color(ColorSpec::new().set_bold(true).set_fg(Some($colour)))?;
+        writeln!($stream, $($arg)+)?;
+        $stream.reset()?;
+    };
+
+}
 
 #[derive(Debug)]
 enum Details {
