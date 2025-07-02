e11ca896e43f673e312612d53b1ab1aa931eaa6b
Brian Li <brianchunghli@gmail.com>
2025-07-02 18:53:48 +1000

    fix: silently fail broken pipes

--- src/main.rs
+++ src/main.rs
@@ -8,11 +8,19 @@ fn main() -> Result<(), Box<dyn Error>> {
     let conf = system::notes_init()?;
     let args = parser::cli().get_matches();
     let command = parser::get_command(&args);
-    match command {
-        Some(("new", args)) => core::create(&conf, args)?,
-        Some(("list", args)) => core::list(&conf, args)?,
-        Some(("config", args)) => core::config(&conf, args)?,
-        _ => {}
-    }
+    if let Err(err) = match command {
+        Some(("new", args)) => core::create(&conf, args),
+        Some(("list", args)) => core::list(&conf, args),
+        Some(("config", args)) => core::config(&conf, args),
+        _ => Ok(()),
+    } {
+        let Some(io_err) = err.downcast_ref::<std::io::Error>() else {
+            return Err(err);
+        };
+        if io_err.kind() == std::io::ErrorKind::BrokenPipe {
+            return Ok(());
+        }
+    };
+
     Ok(())
 }
